cc86f964d210969884278911d83627aee6f07ccf
DeepaKubendiran <carendeepa@gmail.com>
2026-05-26 19:30:00 +0530

    Update output.tf

--- terraform-VPC/output.tf
+++ terraform-VPC/output.tf
@@ -12,3 +12,8 @@ output "public_subnet_id" {
 output "private_subnet_id" {
   value = aws_subnet.private_subnet.id
 }
+
+#Internet Gateway output
+output "internet_gateway_id" {
+  value = aws_internet_gateway.igw.id
+}
