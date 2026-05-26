2dfcbb3f7d9d04cf5cb1f278aa2035bf23d8c124
DeepaKubendiran <carendeepa@gmail.com>
2026-05-26 20:04:10 +0530

    Update main.tf

--- terraform-VPC/main.tf
+++ terraform-VPC/main.tf
@@ -67,3 +67,45 @@ resource "aws_route_table_association" "public_assoc" {
   subnet_id      = aws_subnet.public_subnet.id
   route_table_id = aws_route_table.public_rt.id
 }
+
+#SECURITY GROUP 
+resource "aws_security_group" "jenkins_sg" {
+  name        = var.security_group_name
+  description = "Allow Jenkins, SSH and HTTP traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "SSH Access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Jenkins Access"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP Access"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = var.security_group_name
+  }
+}
