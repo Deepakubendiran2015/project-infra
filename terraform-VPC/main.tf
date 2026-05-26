d7443681b445b98b26c1cb74644dd46b738ac955
DeepaKubendiran <carendeepa@gmail.com>
2026-05-26 19:43:15 +0530

    Update main.tf

--- terraform-VPC/main.tf
+++ terraform-VPC/main.tf
@@ -54,3 +54,16 @@ resource "aws_route_table" "public_rt" {
     Name = "public-route-table"
   }
 }
+
+#PUBLIC INTERNET ROUTE
+resource "aws_route" "public_internet_route" {
+  route_table_id         = aws_route_table.public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+#ASSOCIATE ROUTE TABLE WITH PUBLIC SUBNET
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
