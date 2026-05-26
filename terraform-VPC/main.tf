4d26344480211af7c59508da25947d81fb3184d2
DeepaKubendiran <carendeepa@gmail.com>
2026-05-26 18:45:17 +0530

    Update main.tf

--- terraform-VPC/main.tf
+++ terraform-VPC/main.tf
@@ -1,1 +1,15 @@
+#AWS provider
+provider "aws" {
+  region = "ap-south-1"
+}
 
+#AWS VPC
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = var.vpc_name
+  }
+}
