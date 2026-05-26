c19e18df7ea05f470043ce9f2380039a55c76dae
DeepaKubendiran <carendeepa@gmail.com>
2026-05-26 18:52:48 +0530

    Update main.tf

--- terraform-VPC/main.tf
+++ terraform-VPC/main.tf
@@ -13,3 +13,15 @@ resource "aws_vpc" "main" {
     Name = var.vpc_name
   }
 }
+
+#PUBLIC_SUBNET
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_cidr
+  availability_zone       = "ap-south-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet"
+  }
+}
